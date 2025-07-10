dea0e165da84e371751966e4b4414b0d0508e368
Pinfoxxx <zhirileev.k@gmail.com>
2025-07-10 23:40:06 +0300

    added routers and first structure

---
+++ items_views.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Path
+
+router = APIRouter(prefix="/items", tags=["Items"])
+
+
+@router.get("/")
+def list_items():
+    return [
+        "item1",
+        "item2",
+        "item3",
+        ]
+
+
+# It's important to use right order of endpoints, because if this endpoint was after "/items/{item_id}/", It will be processing by first endpoint and giving error 422, because dict != int
+@router.get("/latest/")
+def get_latest_item():
+    return {"item": {"id": "0", "name": "latest"}}
+
+
+# Endpoint with type annotation
+@router.get("/{item_id}/")
+def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1000000)]):
+    return {
+        "item": {
+            "id": item_id
+        }
+    }
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Path
 from pydantic import EmailStr, BaseModel
 
 import uvicorn
 
+from items_views import router as items_router
+from users.views import router as users_router
+
 app = FastAPI()
-
-
-class CreateUser(BaseModel):
-    email: EmailStr
+app.include_router(items_router)
+app.include_router(users_router)
 
 
 @app.get("/")
@@ -23,15 +26,6 @@ def hello(name: str = "World"): # This func have defaults: "World"
     return {"message": f"Hello {name}"}
 
 
-# Endpoint with email validator from pydantic
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return {
-        "message": "success",
-        "email": user.email,
-    }
-
-
 @app.get("/calc/add")
 def add(a: int, b: int):
     return {
@@ -41,29 +35,7 @@ def add(a: int, b: int):
     }
 
 
-@app.get("/items")
-def list_items():
-    return [
-        "item1",
-        "item2",
-        "item3",
-        ]
 
-
-# It's important to use right order of endpoints, because if this endpoint was after "/items/{item_id}/", It will be processing by first endpoint and giving error 422, because dict != int
-@app.get("/items/latest/")
-def get_latest_item():
-    return {"item": {"id": "0", "name": "latest"}}
-
-
-# Endpoint with type annotation
-@app.get("/items/{item_id}/")
-def get_item_by_id(item_id: int):
-    return {
-        "item": {
-            "id": item_id
-        }
-    }
 
 
 # Uvicorn config for simply script run
